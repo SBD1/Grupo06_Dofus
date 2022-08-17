579a5978340b182f1a2111d8afce5439550553a0
Christian Fleury <chfleurysiq@gmail.com>
2022-08-16 21:07:22 -0300

    refactor: add refactored query

--- src/database/queries.sql
+++ src/database/queries.sql
@@ -1,10 +1,18 @@
 -- query para pegar todas magias de um personagem
-SELECT * FROM magias 
+SELECT magias.nome, magias.descricao, magias.dano, magias.cura FROM magias 
 	INNER JOIN classe ON magias.classe_id = classe.id
 WHERE classe.id = 
 	(SELECT id_classe FROM personagens 
 	INNER JOIN classe ON personagens.id_classe = classe.id
 	WHERE personagens.id = 1);
+
+-- versão reduzida
+SELECT M.nome, M.descricao, M.dano, M.cura FROM magias M
+	INNER JOIN classe C ON M.classe_id = C.id
+WHERE C.id = 
+	(SELECT id_classe FROM personagens P
+	INNER JOIN classe C ON P.id_classe = C.id
+	WHERE P.id = 1);
 
 -- query para pegar o monstro de um mapa
 SELECT * FROM monstro WHERE monstro.id_mapa = 3;

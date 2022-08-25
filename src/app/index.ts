5d376f7ad7c0e30feb6b11769878c263c065afc0
Christian Fleury <chfleurysiq@gmail.com>
2022-08-25 20:31:19 -0300

    feat: export db instance

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,10 @@
+import postgres from 'postgres'
+
 import Game from './game.js'
+import dbConfig from './config/database.js'
+
+const dbInstance = postgres(dbConfig)
 
 new Game()
+
+export default dbInstance

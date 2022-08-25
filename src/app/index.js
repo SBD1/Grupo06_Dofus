824ed3eb1091dd083e63fef05acf569081e2b14f
Christian Fleury <chfleurysiq@gmail.com>
2022-08-25 13:45:36 -0300

    ..

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,5 @@
-var inquirer = require("inquirer");
-console.log({ inquirer: inquirer });
+"use strict";
+exports.__esModule = true;
+var inquirer_1 = require("inquirer");
+console.log({ inquirer: inquirer_1["default"] });
 console.log('jnsdj');

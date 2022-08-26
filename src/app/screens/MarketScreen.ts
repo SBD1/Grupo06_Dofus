02863e545b5f5cdc5042789c008e617743066dbc
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-08-26 09:49:26 -0300

    chore: add market screen

---
+++ src/app/screens/MarketScreen.ts
@@ -0,0 +1,27 @@
+
+
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import dbInstance from "../connection/database.js";
+
+export default class MarketScreen {
+  
+
+  private async showMercatorInfos () {
+    
+  }
+
+  private async showItems () {
+
+  }
+
+  private async buyItem() {
+    
+  }
+
+
+}

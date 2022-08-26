8108931afbe715e2a466fe13f9d750f7c759a912
Christian Fleury <chfleurysiq@gmail.com>
2022-08-26 06:58:42 -0300

    feat: add home screen

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,21 +1,39 @@
-import chalk from 'chalk';
-import inquirer from 'inquirer';
-import gradient from 'gradient-string';
-import chalkAnimation from 'chalk-animation';
-import figlet from 'figlet';
+import chalk from "chalk"
+import inquirer from "inquirer"
+import gradient from "gradient-string"
+import chalkAnimation from "chalk-animation"
+import figlet from "figlet"
 
-import dbInstance from './connection/database.js'
+import dbInstance from "./connection/database.js"
+import { Choices } from "./util/constants.js"
 export default class Game {
-    constructor() {
-        this.run()
-    }
+  constructor() {
+    this.run()
+  }
 
-    private async run () {
-        console.log(dbInstance)
-        
+  private async run() {
+    figlet("DOFUS", "Doom", async (_err, data) => {
+      console.clear()
+      console.log(gradient.pastel.multiline(data) + "\n")
 
-        figlet('DOFUS', 'Doom', (_err, data) => {
-            console.log(gradient.pastel.multiline(data) + '\n');
-          });
-    }
+      const answer = await inquirer.prompt({
+        name: "playOrListAchievements",
+        type: "list",
+        message: "Selecione uma opção.\n",
+        choices: [Choices.PLAY_NOW, Choices.LIST_ACHIEVEMENTS, Choices.QUIT],
+      })
+      console.log(answer.playOrListAchievements)
+
+      switch (answer) {
+        case Choices.PLAY_NOW:
+          break
+        case Choices.LIST_ACHIEVEMENTS:
+          break
+        case Choices.QUIT:
+          break
+        default:
+          break
+      }
+    })
+  }
 }

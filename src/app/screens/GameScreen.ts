fb3a24b51c58ee2805bfdc57260947b7e238122f
Christian Fleury <chfleurysiq@gmail.com>
2022-08-26 09:34:51 -0300

    feat: implement GameScreen structure

--- src/app/screens/GameScreen.ts
+++ src/app/screens/GameScreen.ts
@@ -1,1 +1,31 @@
-console.log('todo')
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import dbInstance from "../connection/database.js";
+import { Mapa } from "../interfaces/mapa.js";
+
+export default class GameScreen {
+    private readonly idPersonagem: number
+
+    constructor(idPersonagem: number){
+        this.idPersonagem = idPersonagem
+    }
+
+
+    async handleGameScreen() {
+        console.clear()
+
+        const currentMap: Mapa = (await dbInstance`
+        SELECT M.id, M.coord_x, M.coord_y, M.descricao, M.mapa_norte, M.mapa_sul, M.mapa_leste, M.mapa_oeste
+         from mapa M 
+         INNER JOIN personagens 
+         p ON m.id = p.id_mapa WHERE p.id = ${this.idPersonagem}
+        `)[0] as any;
+
+        console.log(`(${currentMap.coord_x}, ${currentMap.coord_y}) - ${currentMap.nome}`)
+        console.log()
+        console.log(currentMap.descricao)
+    }
+}

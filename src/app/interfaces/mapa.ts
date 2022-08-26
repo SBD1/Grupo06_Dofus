e6ca5cbd8e53b68fdc84528aa941434d3e150b4c
Christian Fleury <chfleurysiq@gmail.com>
2022-08-26 09:34:39 -0300

    feat: add mapa interface

---
+++ src/app/interfaces/mapa.ts
@@ -0,0 +1,11 @@
+export interface Mapa {
+    id: number
+    coord_x: number
+    coord_y: number
+    nome: number
+    descricao: string
+    mapa_norte: number
+    mapa_sul: number
+    mapa_leste: number
+    mapa_oeste: number
+}

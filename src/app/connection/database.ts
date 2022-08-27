2bac00bac84fedb206adc4bb0152e0e9fb76e535
Christian Fleury <chfleurysiq@gmail.com>
2022-08-27 17:08:40 -0300

    fix: database

--- src/app/connection/database.ts
+++ src/app/connection/database.ts
@@ -1,16 +1,15 @@
-import { Client } from 'pg'
+import postgres from 'postgres'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const client = new Client({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database:  process.env.DATABASE,
-  password:  process.env.PASSWORD,
-  port: Number(process.env.PORT),
-})
+const config = {
+  host: process.env.DBHOST,
+  port: Number(process.env.DBPORT),
+  database: process.env.DBNAME,
+  username: process.env.DBUSER,
+  password: process.env.DBPASS
+}
 
-client.connect()
+const dbInstance = postgres(config)
 
-
-export default client
+export default dbInstance

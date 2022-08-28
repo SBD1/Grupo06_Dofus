1a87f8a9930b883f2695c53db24f8c42068d0798
Christian Fleury <chfleurysiq@gmail.com>
2022-08-28 13:34:00 -0300

    feat: battleScreen structure

---
+++ src/app/screens/BattleScreen.ts
@@ -0,0 +1,50 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import dbInstance from "../connection/database.js";
+
+export default class BattleScreen {
+  private readonly idPersonagem: number;
+  private readonly idNPC: number;
+  constructor({ idPersonagem, idNPC }: { [key: string]: number }) {
+    this.idNPC = idNPC;
+    this.idPersonagem = idPersonagem;
+  }
+
+  async handleBattleScreen() {
+    console.clear();
+
+    const battleStats: {
+      dano: number;
+      nome_arma: string;
+      sorte_total: string;
+      vida_maxima: string;
+    } = (
+      await dbInstance`
+      SELECT A.dano, I.nome AS nome_arma, P.sorte_total, P.vida_maxima FROM personagens P 
+        JOIN instancia_item J ON P.id_arma = J.id
+        JOIN item I ON I.id = J.id_item
+        JOIN arma A ON A.id_item = I.id
+        WHERE P.id = ${this.idPersonagem}`
+    )[0] as any;
+
+    const monsterStats: {
+      moedas: number;
+      vida_maxima: number;
+      dano: number;
+      id_item_recompensa: number;
+      nome: string;
+      descricao: string;
+    } = (
+      await dbInstance`
+    SELECT M.moedas, M.vida_maxima, M.dano, M.id_item_recompensa, N.nome, N.descricao FROM monstro M 
+      JOIN npc N ON N.id = M.id_npc_monstro 
+      WHERE id_npc_monstro = ${this.idNPC};
+    `
+    )[0] as any;
+
+    console.log({ battleStats, monsterStats });
+  }
+}

be9da179c13bf77fe02856d387370a2cd89ecd6e
Christian Fleury <chfleurysiq@gmail.com>
2022-08-27 22:57:23 -0300

    refactor: format document

--- src/app/scripts/populateDb.ts
+++ src/app/scripts/populateDb.ts
@@ -10,7 +10,7 @@ const config = {
   database: process.env.DBNAME,
   username: process.env.DBUSER,
   password: process.env.DBPASS,
-  max: 1
+  max: 1,
 };
 
 const __filename = fileURLToPath(import.meta.url);
@@ -18,26 +18,30 @@ const dbInstance = postgres(config);
 
 (async () => {
   try {
-    await dbInstance.file(   `${path.resolve(
-      path.dirname(__filename),
-      "..",
-      "..",
-      "src",
-      "database",
-      "ddl.sql"
-    )}`);
+    await dbInstance.file(
+      `${path.resolve(
+        path.dirname(__filename),
+        "..",
+        "..",
+        "src",
+        "database",
+        "ddl.sql"
+      )}`
+    );
 
-    await dbInstance.file(   `${path.resolve(
-      path.dirname(__filename),
-      "..",
-      "..",
-      "src",
-      "database",
-      "dml.sql"
-    )}`);
+    await dbInstance.file(
+      `${path.resolve(
+        path.dirname(__filename),
+        "..",
+        "..",
+        "src",
+        "database",
+        "dml.sql"
+      )}`
+    );
   } catch (err) {
     console.log(err);
   }
 })();
 
-process.exit(0)
+process.exit(0);

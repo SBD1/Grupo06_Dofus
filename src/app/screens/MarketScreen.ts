92ab668843466cc48efde09f1269e7b1b82452fd
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-08-29 09:06:08 -0300

    feat: add  query to find all items from a merchant

--- src/app/screens/MarketScreen.ts
+++ src/app/screens/MarketScreen.ts
@@ -12,20 +12,23 @@ export default class MarketScreen {
   constructor(merchant_id: number) {
     this.merchant_id = merchant_id
   }
-  
+
   private readonly merchant_id
 
   private async showMercatorInformations () {
     const merchant = await dbInstance`
       SELECT * FROM npc WHERE id = ${this.merchant_id}
     `;
-    
+
     console.log(merchant)
   }
   private async showItems () {
+    const items = await dbInstance`
+      SLECT * FROM npc_mercador_itens WHERE id_npc_mercador = ${this.merchant_id}
+    `
+    console.log(items)
 
   }
-
   private async buyItem() {
     
   }

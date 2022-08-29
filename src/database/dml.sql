3d61ae0daba5b6a722f4605e9b2da104355c3e00
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-08-29 20:43:19 -0300

    fix: use serial to insert id

--- src/database/dml.sql
+++ src/database/dml.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
-INSERT INTO mapa (id, coord_x, coord_y, descricao, nome) VALUES
-(1, 0, 0, 'Seja bem vinda jovem alma, você acaba de incarnar no mundo dos Doze. Você se encontra em um templo de pedras claras, com detalhes em branco, e dourado. Algumas pedras flutuam a sua volta.', 'Templo das Almas'),
-(2, 1, 0, 'Você se encontra na saída do templo de pedras, em suas costas há uma grande porta de madeira, e em sua frente um vasto campo de trigos, um tapete dourado. Entre eles uma estrada, o chamado caminho das almas.', 'Caminho das Almas'),
-(3, 2, 0, 'Você se encontra em um uma espécie de feira, há várias pessoas comprando os mais diversos produtos. há algumas estradas que vão para todas as direções. No sul, uma floresta escura, no norte, um campo dourado.', 'Feira de Incarnam'),
-(4, 2, 1, 'Você se encontra em um grande pasto dourado, há algumas plantações distantes, e alguns papatudos com a lâ branca, quase prateada, a sua volta.', 'Pastos de Incarnam'),
-(5, 2, -1, 'Você se encontra nos limítes da floresta sombria, mas não exatamente dentro dela. Ainda há bastante luz entre as árvores de folhas escuras, mas você já consegue ouvir lobos uivando.', 'Limite da Floresta Sombria'),
-(6, 3, 0, 'Você se encontra em um lago bem azul, cercado pela grama dourada. Você vê alguns peixes nadando, e algumas pessoas pescando.', 'Lago de Incarnam');
+INSERT INTO mapa (coord_x, coord_y, descricao, nome) VALUES
+(0, 0, 'Seja bem vinda jovem alma, você acaba de incarnar no mundo dos Doze. Você se encontra em um templo de pedras claras, com detalhes em branco, e dourado. Algumas pedras flutuam a sua volta.', 'Templo das Almas'),
+(1, 0, 'Você se encontra na saída do templo de pedras, em suas costas há uma grande porta de madeira, e em sua frente um vasto campo de trigos, um tapete dourado. Entre eles uma estrada, o chamado caminho das almas.', 'Caminho das Almas'),
+(2, 0, 'Você se encontra em um uma espécie de feira, há várias pessoas comprando os mais diversos produtos. há algumas estradas que vão para todas as direções. No sul, uma floresta escura, no norte, um campo dourado.', 'Feira de Incarnam'),
+(2, 1, 'Você se encontra em um grande pasto dourado, há algumas plantações distantes, e alguns papatudos com a lâ branca, quase prateada, a sua volta.', 'Pastos de Incarnam'),
+(2, -1, 'Você se encontra nos limítes da floresta sombria, mas não exatamente dentro dela. Ainda há bastante luz entre as árvores de folhas escuras, mas você já consegue ouvir lobos uivando.', 'Limite da Floresta Sombria'),
+(3, 0, 'Você se encontra em um lago bem azul, cercado pela grama dourada. Você vê alguns peixes nadando, e algumas pessoas pescando.', 'Lago de Incarnam');
 
 UPDATE mapa SET mapa_leste = 2 WHERE id=1;
 UPDATE mapa SET mapa_leste = 3 WHERE id=2;
@@ -16,11 +16,11 @@ UPDATE mapa SET mapa_norte = 3 WHERE id=5;
 UPDATE mapa SET mapa_oeste = 3 WHERE id=6;
 
 INSERT INTO classe (id, nome, descricao, vida_inicial, sorte) VALUES
-(1, 'Eniripsa', 'Os Eniripsas são curandeiros capazes de curar com uma simples palavra. Eles usam o poder das palavras para aliviar o sofrimento dos seus aliados, mas também para ferir seus inimigos.', 3000, 50),
-(2, 'Iop', 'Os Iops são guerreiros intrépidos e temerários! Uma coisa é certa: os Iops sabem usar suas armas como ninguém. Para eles, entrar em uma briga ao menos uma vez por dia é sinal de boa saúde.', 3000, 10),
-(3, 'Sacrier', 'Os Sacriers são berserkers capazes de multiplicar sua força cada vez que são atacados! Como não têm medo de receber golpes nem de se expor ao perigo, eles costumam ficar na linha de frente, prontos para derramar o sangue dos inimigos!', 5000, 25),
-(4, 'Ecaflip', 'Os Ecaflips são guerreiros apostadores que vivem enfiados em lugares onde é possível ganhar uma grana preta e perder tudo... Um bom Ecaflip joga sem parar e sempre aposta no tudo ou nada.', 1500, 95),
-(5, 'Feca', 'Os Fecas são protetores leais e ficam sempre na defensiva. Eles são muito apreciados pelos grupos de aventureiros graças às suas armaduras elementares capazes de suportar até mesmo os golpes mais fortes.', 8000, 40);
+('Eniripsa', 'Os Eniripsas são curandeiros capazes de curar com uma simples palavra. Eles usam o poder das palavras para aliviar o sofrimento dos seus aliados, mas também para ferir seus inimigos.', 3000, 50),
+('Iop', 'Os Iops são guerreiros intrépidos e temerários! Uma coisa é certa: os Iops sabem usar suas armas como ninguém. Para eles, entrar em uma briga ao menos uma vez por dia é sinal de boa saúde.', 3000, 10),
+('Sacrier', 'Os Sacriers são berserkers capazes de multiplicar sua força cada vez que são atacados! Como não têm medo de receber golpes nem de se expor ao perigo, eles costumam ficar na linha de frente, prontos para derramar o sangue dos inimigos!', 5000, 25),
+('Ecaflip', 'Os Ecaflips são guerreiros apostadores que vivem enfiados em lugares onde é possível ganhar uma grana preta e perder tudo... Um bom Ecaflip joga sem parar e sempre aposta no tudo ou nada.', 1500, 95),
+('Feca', 'Os Fecas são protetores leais e ficam sempre na defensiva. Eles são muito apreciados pelos grupos de aventureiros graças às suas armaduras elementares capazes de suportar até mesmo os golpes mais fortes.', 8000, 40);
 
 INSERT INTO magias (classe_id, nome, descricao, dano, cura) VALUES
 (1, 'Palavra que fere', 'Uma magia que causa danos em seus inimigos através da palavra.', 200, 0),
@@ -69,7 +69,7 @@ CALL cria_novo_amuleto('Corujamuleto', 'Dizem que este amuleto permite que seu p
 CALL cria_novo_amuleto('Amuleto de Safira', 'Dizem que este um dos mais belo amuletos já vistos no mundo dos dose, e sua beleza é proporcional a sorte', 600, 700)
 
 
-INSERT INTO instancia_item(id, id_item) VALUES
+INSERT INTO instancia_item(id_item) VALUES
 (1, 1),
 (2, 6),
 (3, 11),
@@ -77,29 +77,29 @@ INSERT INTO instancia_item(id, id_item) VALUES
 (5, 13),
 (6, 14);
 
-INSERT INTO personagens (id, id_classe, moedas, id_mapa) VALUES
-(1, 1, 40, 1);
+INSERT INTO personagens (id_classe, moedas, id_mapa) VALUES
+(1, 40, 1);
 
 INSERT INTO mochila (id_personagem, id_instancia_item) VALUES
 (1, 4),
 (1, 5),
 (1, 6);
 
-INSERT INTO npc (id, nome, tipo_npc, descricao, id_mapa) VALUES
-(1, 'Maurí Sioserrano', 'npc_missao', 'Maurí é um guerreiro experiente, que possui várias cicatrizes de batalha, você sente que ele pode te ensinar sobre magia.', 6),
-(2, 'Afonso', 'mercador', 'O maior varegista de Astrub', 2),
-(3, 'Chupa Cabra', 'monstro', 'Criatura sanguinaria destruidora de vilas', 3),
-(4, 'Papatudo', 'monstro', 'Essa criatura é reponsável por proteger os campos de Astrub e Incarnan', 3),
-(5, 'Piwi', 'monstro', 'Pequenina ave colorida, porem poderosa', 3),
-(6, 'Lobo', 'monstro', 'Criatura uivante maluca e assassina', 3),
-(7, 'Prespic', 'monstro', 'Espinhoso monstro venenoso', 3);
+INSERT INTO npc (nome, tipo_npc, descricao, id_mapa) VALUES
+('Maurí Sioserrano', 'npc_missao', 'Maurí é um guerreiro experiente, que possui várias cicatrizes de batalha, você sente que ele pode te ensinar sobre magia.', 6),
+('Afonso', 'mercador', 'O maior varegista de Astrub', 2),
+('Chupa Cabra', 'monstro', 'Criatura sanguinaria destruidora de vilas', 3),
+('Papatudo', 'monstro', 'Essa criatura é reponsável por proteger os campos de Astrub e Incarnan', 3),
+('Piwi', 'monstro', 'Pequenina ave colorida, porem poderosa', 3),
+('Lobo', 'monstro', 'Criatura uivante maluca e assassina', 3),
+('Prespic', 'monstro', 'Espinhoso monstro venenoso', 3);
 
-INSERT INTO missao (id, id_npc_missao, descricao, moedas, id_item_missao, id_item_recompensa) VALUES
-(1, 1, 'Encontre uma semente de gergelim para completar essa missão, geralmente os Piwis gostam muito dessas sementes.', 40, 11, NULL),
-(2, 1, 'Encontre um pelo de rato, esse animais gostam de lugares escuros.', 200, 12, 3),
-(3, 1, 'Encontre o testículo de um lobo raivoso.', 500, 13, NULL),
-(4, 1, 'Encontre um pouco de lã de papatudo, geralmente eles gostam de ficar nos campos.', 200, 14, NULL),
-(5, 1, 'Encontre pedregulho do rochedo.', 5000, 15, NULL);
+INSERT INTO missao (id_npc_missao, descricao, moedas, id_item_missao, id_item_recompensa) VALUES
+(1, 'Encontre uma semente de gergelim para completar essa missão, geralmente os Piwis gostam muito dessas sementes.', 40, 11, NULL),
+(1, 'Encontre um pelo de rato, esse animais gostam de lugares escuros.', 200, 12, 3),
+(1, 'Encontre o testículo de um lobo raivoso.', 500, 13, NULL),
+(1, 'Encontre um pouco de lã de papatudo, geralmente eles gostam de ficar nos campos.', 200, 14, NULL),
+(1, 'Encontre pedregulho do rochedo.', 5000, 15, NULL);
 
 UPDATE missao SET id_missao_desbloqueada = 2 WHERE id=1;
 UPDATE missao SET id_missao_desbloqueada = 3 WHERE id=2;
